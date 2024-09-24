f06a3bd008bb033624db2bd95aa2b4136f05350d
peter-duongGhub <pduong197@icloud.com>
2024-09-24 21:22:16 +0100

    added all comments to player controller

--- src/controllers/player_controller.py
+++ src/controllers/player_controller.py
@@ -19,7 +19,7 @@ from controllers.event_controller import event_bp
 player_bp = Blueprint("player", __name__, url_prefix="/<int:game_id>/players")
 @player_bp.register_blueprint(event_bp)
 
-# POST (CREATE) - Create Player into databse from a HTTP Request
+# Create a route to create a player object into the database
 @player_bp.route("/", methods=["POST"])
 def create_player(game_id, user_id):
     try:
@@ -62,7 +62,7 @@ def create_player(game_id, user_id):
         return{"error" : "Please enter date in the correct format yyyy-mm-dd or yyyy-mm-dd."}
         
 
-# Update player's name
+# Create a route to update a player objects inside the database name
 @player_bp.route("/<int:player_id>", methods=["PUT", "PATCH"])
 @jwt_required()
 def update_player(player_id, game_id, user_id): 
@@ -79,61 +79,69 @@ def update_player(player_id, game_id, user_id):
         if player is None:
             return{"error": "No such player exists"}, 404
         
-        # Update user attributes if provided
+        # If there is a player with id equal to the player id:
         if player:
+            # Change the player name with id equal to player id to the front-end input name and commit to the database session
             player.name = name or player.name
-
             db.session.commit()
 
-        # Return the updated user information
+        # Return the updated player information to the view owith a success code 200
         return player_schema.dump(player), 200
+    # If there is no name input return an error message
     else:
         return {"error" : "Please enter a new player name to update existing player name"}
     
-# delete player
+# Create a route to delete a specific player depending on the id of the player in the dynamic route
 @player_bp.route("/<int:player_id>", methods=["DELETE"])
 @jwt_required()
 def delete_user(player_id, user_id, game_id):
     try:
-        # Attempt to retrieve the user with the given id
+        # Retrieve the Player object with the specified player id in the dynamic route
         stmt = db.Select(Players).filter_by(id=player_id)
         player = db.session.scalar(stmt)
 
-        # If user is found, delete and commit
+        # If there is a player object with the specific player id delete the player and commit the change to the database session
         if player:
             db.session.delete(player)
             db.session.commit()
-            # Return a success message
+            # Return a success message of the deleted player id 
             return (f"Player with id {player_id} is deleted.")
         
-        # If user is not found, return a 404 error
+        # If there is no such player with the player id return error message
         else:
             return (f"Player with id {player_id} not found."), 404
-    
+    # General Error handling for any unexpected errors
     except Exception as e:
-        # Handle any unexpected errors
         return (str(e)), 500
     
 
-# View all players
+# Create an endpoint to view all players
 @player_bp.route("/", methods=["GET"])
 def view_players(game_id, user_id):
+    # Fetch all player objects from the database
     stmt = db.select(Players)
     player = db.session.scalars(stmt)
 
+    # If there are player objects:
     if player:
+        # Provide the view with deserialised player objects
         return players_schema.dump(player), 201
+    # If there isnt return an error message
     else:
         return {"error" : "There are no players to show"}
     
-# View specific players
+# Create an endpoint to view specific player
 @player_bp.route("/<int:player_id>", methods=["GET"])
 def specific_players(player_id, game_id, user_id):
+    # Fetch specific player from the database depending on the id in the dynamic route
     stmt = db.select(Players).filter_by(id=player_id)
     player = db.session.scalar(stmt)
 
+    # If there is a player with the player id in the dynamic route:
     if player:
-        return player_schema.dump(player), 201
+        # Return to the view the specific player object deserialised with a success code
+        return player_schema.dump(player), 200
     else:
+        # Return an error message if there is no player with the specific id
         return {"error" : f"Player with id {player_id} can not be found."}
     
